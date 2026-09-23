71ea69e1a4e9138fc1f6dcfbaacbdf38ba8c5261
Chaithanya <chaithanya2996@gmail.com>
2026-09-23 08:55:34 +0530

    updated day-2

--- Day-2/02.main.tf
+++ Day-2/02.main.tf
@@ -1,4 +1,19 @@
-resource "aws_instance" "myinstance" {
+resource "aws_instance" "myinstance1" {
   ami           = "ami-066c4849e6b3a1e3d"
   instance_type = "t3.micro"
 }
+resource "aws_instance" "myinstance2" {
+  ami           = "ami-066c4849e6b3a1e3d"
+  instance_type = "t3.micro"
+}
+
+resource "aws_instance" "myinstance3" {
+  ami           = "ami-066c4849e6b3a1e3d"
+  instance_type = "t3.micro"
+}
+
+resource "aws_instance" "myinstance4" {
+  ami           = "ami-066c4849e6b3a1e3d"
+  instance_type = "t3.micro"
+}
+
